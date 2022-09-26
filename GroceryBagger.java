b5d710fd17cbf417a8e58713478d6c0b5a701dda
Darrian Yang <yangd2@wit.edu>
2022-09-26 09:59:21 -0400

    Classes
    3 Possible classes for use, GroceryBagger class to represent a bagger in a grocery store that organizes the items into the bag. GroceryBag class to represent the bag itself where the items will go into. GroceryItem class to represent the description and the attributes of the item.

---
+++ GroceryBagger.java
@@ -0,0 +1,13 @@
+package edu.wit.scds.ds.bag.app;
+
+/** @author 
+ * 
+ * Class of GrocceryBagger class representing the organizing of items in bag 
+ * 
+ * @author yangd2
+ *
+ */
+
+public class GroceryBagger {
+
+}
---
+++ GroceryItem.java
@@ -0,0 +1,16 @@
+package edu.wit.scds.ds.bag.app;
+
+/** @author  
+ * 
+ * Demonstration of class GroceryItem
+ * 
+ * @author yangd2
+ *
+ */
+
+public class GroceryItem {
+
+    String name ; 
+    
+    
+}
