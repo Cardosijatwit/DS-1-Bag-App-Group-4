6aa7fbec9279d8c383f24a0c04de88891884fc9d
Darrian Yang <yangd2@wit.edu>
2022-09-26 09:30:51 -0400

    Adding 3 classes
    GroceryItem class, ItemWeight enum, GroceryBag class

---
+++ ItemWeight.java
@@ -0,0 +1,13 @@
+package edu.wit.scds.ds.bag.app;
+
+
+/** @author 
+ * 
+ * Enumeration of grocery item weight for the GroceryBagger application 
+ * 
+ * @author yangd2
+ *
+ */
+public enum ItemWeight {
+
+}
