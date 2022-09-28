dc68d92b9a3910de486a7ead5387a57cc5aac1a7
cardosij <cardosij@PF2XVEVD.wit.private>
2022-09-28 14:51:05 -0400

    changes

--- DS = 1 Bag App = Group 4/src/edu/wit/scds/ds/bag/app/GroceryItemFlexibility.java
+++ DS = 1 Bag App = Group 4/src/edu/wit/scds/ds/bag/app/GroceryItemFlexibility.java
@@ -3,56 +3,68 @@ package edu.wit.scds.ds.bag.app;
 public enum GroceryItemFlexibility
 	{
 	FLEXIBLE("Flexible", 1),
-	RIGID("Rigid", 2);
+	RIGID("Rigid", 2) ;
 
-	public final String displayName;
-	public final int flexibilityValue;
+	public final String displayName ;
+	public final int flexibilityValue ;
 
-	private GroceryItemFlexibility(final String itemFlexibilityDisplayName, final int itemFlexibilityValue)
+	private GroceryItemFlexibility( final String itemFlexibilityDisplayName, final int itemFlexibilityValue )
 		{
-		this.displayName = itemFlexibilityDisplayName;
-		this.flexibilityValue = itemFlexibilityValue;
+		this.displayName = itemFlexibilityDisplayName ;
+		this.flexibilityValue = itemFlexibilityValue ;
 
 		}
 
-	public static GroceryItemFlexibility interpretDescription(final String flexibilityDescription)
+	public static GroceryItemFlexibility interpretDescription( final String flexibilityDescription )
 		{
 		GroceryItemFlexibility correspondingFlexibility;
-		switch(flexibilityDescription.toLowerCase().charAT(0))
+		switch( flexibilityDescription.toLowerCase().charAt( 0 ) )
 			{
-			case ‘f’:
-				correspondingFlexibility = FLEXIBLE;
-				break;
-			case ‘r’:
-				correspondingFlexibility = RIGID;
-				break;
+			case 'f':
+				correspondingFlexibility = FLEXIBLE ;
+				break ;
+			case 'r':
+				correspondingFlexibility = RIGID ;
+				break ;
 			default:
-				correspondingFlexibility = FLEXIBLE;
-				break;
+				correspondingFlexibility = FLEXIBLE ;
+				break ;
 			}
-return correspondingFlexibility;
-}
-public String to String(){
-return this.displayName;
-}
-public static void main(final String[] args){
-System.out.printf(“Members of the %s enumeration%n%n”,
-GroceryItemFlexibility.class.getSimpleName());
-System.out.printf(“%-5s %-15s %-15s %-15s %-15s %-15s%n”,
-“#”,
-“Item Flexibility”,
-“Name”,
-“Display Name”,
-“Flexibility Value”,
-“Interpreted Flexibility”);
-for (final GroceryItemFlexibility anItemFlexibility: GroceryItemFlexibility.values()){
-System.out.printf(“%-5s %-15s %-15s %-15s %-15s %-15s%n”,
-anItemFlexibility.ordinal(),
-anItemFlexibility,
-anItemFlexibility.name(),
-anItemFlexibility.displayName,
-anItemFlexibility.flexibilityValue,
-interpretDescription(anItemFlexibility.toString()));
-}
-}
-}
+		
+		return correspondingFlexibility;
+		
+		}
+	public String toString()
+		{
+		return this.displayName;
+		
+		}
+	
+	public static void main(final String[] args)
+		{
+		System.out.printf("Members of the %s enumeration%n%n",
+		GroceryItemFlexibility.class.getSimpleName()) ;
+		
+		System.out.printf("%-5s %-15s %-15s %-15s %-15s %-15s%n",
+						  "#",
+						  "Item Flexibility",
+						  "Name",
+						  "Display Name",
+						  "Flexibility Value",
+						  "Interpreted Flexibility") ;
+		
+		for (final GroceryItemFlexibility anItemFlexibility: GroceryItemFlexibility.values())
+			{
+			System.out.printf("%-5s %-15s %-15s %-15s %-15s %-15s%n",
+							  anItemFlexibility.ordinal(),
+							  anItemFlexibility,
+							  anItemFlexibility.name(),
+							  anItemFlexibility.displayName,
+							  anItemFlexibility.flexibilityValue,
+							  interpretDescription(anItemFlexibility.toString()));
+			
+			}
+		
+		}
+	
+	}
