f0daf0b4d32e7fd3404f908a610a725d8a4c5a2a
cardosij <cardosij@PF2XVEVD.wit.private>
2022-09-28 15:35:51 -0400

    test stuff

--- DS = 1 Bag App = Group 4/src/edu/wit/scds/ds/bag/app/Testing_stuff.java
+++ DS = 1 Bag App = Group 4/src/edu/wit/scds/ds/bag/app/Testing_stuff.java
@@ -1,10 +1,38 @@
-package edu.wit.scds.ds.bag.app;
 
-public class Testing_stuff {
+package edu.wit.scds.ds.bag.app ;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import java.util.Scanner ;
+import java.io.File ;
+import java.io.FileNotFoundException;
+
+
+
+public class Testing_stuff 
+	{
+
+	public static void main(String[] args) 
+		{
+		
+		try 
+			{
+			Scanner grocerylistreader = new Scanner( new File( "./data/groceries.txt" ) ) ;
+			String line = grocerylistreader.nextLine() ;
+			String[] splits = line.split("\t") ;
+			for (int i = 0; i < splits.length; i++ ) {
+				System.out.println(splits[i]) ;
+			}
+			
+			// while (grocerylistreader.hasNextLine()) 
+				{
+				
+				}
+			}
+		
+		catch (FileNotFoundException e) 
+			{
+			System.out.printf( "The file defined by the path \"./data/groceries.txt\" was not found" ) ;
+			}
+		
+		}
 
 	}
-
-}
