783bbd7be87c9cd8451f98a942ce55f8e0c4f563
Darrian Yang <yang2@wit.edu>
2022-09-28 14:50:07 -0400

    created the code to read in groceries.txt file next action is to implement the string split with item.split

--- DS = 1 Bag App = Group 4/src/edu/wit/scds/ds/bag/app/GroceryItems.java
+++ DS = 1 Bag App = Group 4/src/edu/wit/scds/ds/bag/app/GroceryItems.java
@@ -1,10 +1,39 @@
 package edu.wit.scds.ds.bag.app;
 
-public class GroceryItems {
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
 
-    }
+/**
+ * 
+ * class that will take the arrays that are separated from the groceries.txt file
+ * and put them into the actual item names 
+ * 
+ * @author yangd2
+ *
+ */
 
+public class GroceryItems 
+{
+
+    public static void main(String[] args)
+    {
+        File file = new File ("./data/groceries.txt");
+        
+        // try and catch block to read the groceries.txt file into the class 
+        try (Scanner sc = new Scanner(file))
+                {
+                  while (sc.hasNextLine()) {
+                      System.out.println(sc.nextLine()); 
+                  }
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+        
+     }
 }
+        
+        
+
